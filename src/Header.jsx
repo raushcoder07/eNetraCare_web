6ab3055fe068a89cc9ed797f22fdffae8b050f8a
Raushan Kumar <raushan.shs@gmail.com>
2024-06-06 20:56:48 +0530

    add submenu

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import { Menu } from "antd";
+const { SubMenu } = Menu;
 
 const Header = () => {
   return (
@@ -11,18 +13,43 @@ const Header = () => {
           <p className="tagline">AI-Powered Eye Health & Cataract Detection</p>
         </div>
         <nav className="nav">
-          <ul>
-          <li><Link to="/">Home</Link></li>
-            <li><Link to="/about">About Us</Link></li>
-            <li><Link to="/services">Services</Link></li>
-            <li><Link to="/ai-detection">AI Detection</Link></li>
-            <li><Link to="/cataracts">Cataracts</Link></li>
-            <li><Link to="/resources">Patient Resources</Link></li>
-            <li><Link to="/contact">Contact Us</Link></li>
-          </ul>
+          <Menu mode="horizontal">
+            <Menu.Item key="home">
+              <Link to="/">Home</Link>
+            </Menu.Item>
+            <Menu.Item key="about">
+              <Link to="/about">About Us</Link>
+            </Menu.Item>
+            <Menu.Item key="services">
+              <Link to="/services">Services</Link>
+            </Menu.Item>
+            <Menu.Item key="ai-detection">
+              <Link to="/ai-detection">AI Detection</Link>
+            </Menu.Item>
+            <Menu.Item key="cataracts">
+              <Link to="/cataracts">Cataracts</Link>
+            </Menu.Item>
+
+            <SubMenu key="patient-resource" title="Patient Resource">
+              <Menu.Item key="resources">
+                <Link to="/resources">Testimonials</Link>
+              </Menu.Item>
+              <Menu.Item key="resources">
+                <Link to="/resources">Information</Link>
+              </Menu.Item>
+              <Menu.Item key="resources">
+                <Link to="/resources">News</Link>
+              </Menu.Item>
+              <Menu.Item key="resources">
+                <Link to="/resources">Eye Care tips</Link>
+              </Menu.Item>
+            </SubMenu>
+
+            <Menu.Item key="contact">
+              <Link to="/contact">Contact Us</Link>
+            </Menu.Item>
+          </Menu>
         </nav>
-        {/* <div className="header-right">
-        </div> */}
       </div>
     </header>
   );

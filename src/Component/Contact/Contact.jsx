67ebea215ef512b45f1e47f9ddd75e0e7d400d39
Raushan Kumar <159451682+raushcoder07@users.noreply.github.com>
2024-06-15 12:02:48 +0530

    addGoogle_map

--- src/Component/Contact/Contact.jsx
+++ src/Component/Contact/Contact.jsx
@@ -1,54 +1,40 @@
-import React from 'react'
-import './ContactCSS.css'
-import contactImg from './images/contact.png'
+import React from 'react';
+import './ContactCSS.css';
+// import contactImg from './images/contact.png'; // Uncomment and provide the correct path to your image
 
 const Contact = () => {
   return (
-    // <div classNameName="contact-container">
-    //   <div classNameName="contact-content">
-    //     <h1 id='contact-title'>Connect with us  </h1>
-    //     <div>
-    //       <div classNameName='contact-details'>
-    //       <i className="fa-solid fa-phone"></i>
-    //       <p id='contact-number'>+1-423-826-6231</p>
-    //       </div>
-
-    //       <div classNameName='contact-details'>
-    //       <i className="fa-solid fa-location-dot"></i>
-    //       <div><p id='contact-address'> 998 Broadway Avenue <br /> Zip:19054, Levittown <br />Pennsylvania,United States</p></div>
-
-    //       </div>
-
-    //     </div>
-    //   </div>
-    // </div>
-
-    <div className='contact-page'>
+    <div className="contact-page">
       <div className="contact-container">
-        <div className="row" style={{width:'50%', margin: '8% 0'}}>
-          <div className="col-md-8">
-            <address>
-              <strong style={{ fontSize: '2rem', marginBottom: '10px' }}>Innovease India Private Limited.</strong><br />
-              P NO 27, MOTI DOONGRI YOJANA,<br />
-              GOVIND MARG,<br />
-              Jaipur, Rajasthan, India, 302004<br />
-              Year of Founding: 2021<br />
-              Phone: +91-9923795140 / +91-9284197126
-            </address>
-
-            <address>
-              <strong>Email <i className="fa-regular fa-envelope"></i></strong><br />
-              <a href="mailto:#"> innoveaseindia@rediffmail.com </a>
-              <a href="mailto:#"> aabhamaikar1982@gmail.com</a>
-            </address>
-          </div>
+        <div className="row">
+          <address>
+            <strong style={{ fontSize: '2rem', marginBottom: '10px' }}>Innovease India Private Limited.</strong><br />
+            P NO 27, MOTI DOONGRI YOJANA,<br />
+            GOVIND MARG,<br />
+            Jaipur, Rajasthan, India, 302004<br />
+            Year of Founding: 2021<br />
+            Phone: +91-9923795140 / +91-9284197126
+          </address>
+          <address>
+            <strong>Email <i className="fa-regular fa-envelope"></i></strong><br />
+            <a href="mailto:innoveaseindia@rediffmail.com">innoveaseindia@rediffmail.com</a>
+            <a href="mailto:aabhamaikar1982@gmail.com">aabhamaikar1982@gmail.com</a>
+          </address>
         </div>
-        <div className="contact-img-container">
-          <img src={contactImg} className='contact-img' />
+        
+        <div className="google-map-container">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3558.154063706936!2d75.81483931450769!3d26.872315668382107!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x396db4a5f4d5ed8f%3A0xe9a9cde4814b0ed4!2sInnovease%20India%20Private%20Limited!5e0!3m2!1sen!2sin!4v1622543421470!5m2!1sen!2sin"
+            width="100%"
+            height="100%"
+            allowFullScreen=""
+            loading="lazy"
+            title="Google Map"
+          ></iframe>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
